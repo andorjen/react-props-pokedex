f27b20463e1edf5473b2256ad730353c1ceac68c
Colin Sidberry <colinsidberry@gmail.com>
2021-10-11 15:33:56 -0700

    wrote Pokecard component

---
+++ src/Pokecard.js
@@ -0,0 +1,14 @@
+function Pokecard({ id, name, type, base_experience }) {
+    return (
+        <div className="Pokecard">
+            <h3 className="Pokecard-name">{name}</h3>
+            <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
+                alt=""
+            />
+            <p>Type: {type}</p>
+            <p>EXP: {base_experience}</p>
+        </div>
+    );
+}
+
+export default Pokecard;
